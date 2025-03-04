a005bd8cee89ed3b797a4eadf417259d7a175f2f
Patthethicc <167599442+Patthethicc@users.noreply.github.com>
2025-03-04 22:13:47 +0800

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -7,8 +7,6 @@ import {
   deleteImageById,
 } from "../services/file.js";
 import { userLogIn, userSignUp } from "../services/authController.js";
-import { db } from "../models/Comment.js";
-import Post from "../models/Post.js";
 
 
 const router = express.Router();
