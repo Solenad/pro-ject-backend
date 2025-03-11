1cba471abf0867e1724618030cb3b18dfc16c039
Andrei Zarmin De Jesus <161214656+DreiZzzzz@users.noreply.github.com>
2025-03-11 18:42:58 +0800

    feat(comments): add comment endpoints for crud

--- routes/routes.js
+++ routes/routes.js
@@ -15,6 +15,18 @@ import {
 import { userLogIn, userSignUp } from "../services/auth.js";
 import { createUser, getUser, getUsers } from "../services/user.js";
 
+// set endpoints for comment functions
+import {
+  addParentComment,
+  getParentComments,
+  addReplyComment,
+  getReplyComments,
+  editComment,
+  deleteComment,
+  upvoteComment,
+  downvoteComment,
+} from "../services/comment.js";
+
 const router = express.Router();
 
 router.get("/test", function (req, res) {
@@ -41,5 +53,19 @@ router.delete("/image/:id", deleteImageById);
 // auth endpoints
 router.post("/auth/login", userLogIn); // from auth to /auth/login
 router.post("/auth/signup", userSignUp); // from auth to /auth/signup
+
+// comment endpoints
+// comment endpoints
+router.post("/comments", addParentComment); // Add a new parent comment
+router.get("/comments/:post_id", getParentComments); // Get top-level comments for a post
+
+router.post("/comments/reply", addReplyComment); // Add a reply to a comment
+router.get("/comments/replies/:comment_id", getReplyComments); // Get replies for a comment
+
+router.put("/comments/:comment_id", editComment); // Edit a comment
+router.delete("/comments/:comment_id", deleteComment); // Delete a comment
+
+router.patch("/comments/:comment_id/upvote", upvoteComment); // Upvote a comment
+router.patch("/comments/:comment_id/downvote", downvoteComment); // Downvote a comment
 
 export default router;
