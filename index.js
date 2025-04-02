d1bcc3fdeb1ea528f726ecaacaa06e79f8c62d70
Solenad <166220458+Solenad@users.noreply.github.com>
2025-04-02 16:28:27 +0800

    chore: set secure to true

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: false, // Set to true if using HTTPS
+      secure: true, // Set to true if using HTTPS
       maxAge: 60 * 60 * 1000, // 1 hour in milliseconds
     },
   })
